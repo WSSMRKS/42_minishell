81eb13551c3cab45553f3791e0a80c43c984180f
Markus Weiss <markusweiss.mw@gmail.com>
2024-08-16 08:55:46 +0200

    prototype of echo

---
+++ builtins/echo.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 12:43:05 by maweiss           #+#    #+#             */
+/*   Updated: 2024/08/15 13:09:08 by maweiss          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char *_echo(char *msg)
+{
+	
+	// read message
+	// identify quotes
+	// implement variable expansion.
+	
+}
+
+int	main(void)
+{
+	printf("bla\\n");
+	// echo("this is the message to echo. This is an env. variable $BLA");
+	// echo("this is the message to echo. This is an env. variable $BLA");
+	// echo("this is the message to echo. This is an env. variable $BLA");
+	// echo("this is the message to echo. This is an env. variable $BLA");
+	// echo("this is the message to echo. This is an env. variable $BLA");
+	// echo("this is the message to echo. This is an env. variable $BLA");
+}
