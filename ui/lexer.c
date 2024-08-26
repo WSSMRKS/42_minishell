ea14f2d174f621370532a8a9feb5f4ed13072828
dafneko <defne.koca@outlook.com>
2024-08-26 12:37:47 +0200

    lexer

--- ui/lexer.c
+++ ui/lexer.c
@@ -6,7 +6,7 @@
 /*   By: dkoca <dkoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 06:07:52 by dkoca             #+#    #+#             */
-/*   Updated: 2024/08/26 08:26:36 by dkoca            ###   ########.fr       */
+/*   Updated: 2024/08/26 12:30:13 by dkoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,39 +20,60 @@ int is_not_end(const char *itr)
 	return (FALSE);
 }
 
-int lookahead(const char *chr_itr)
+int lookahead(const char *chr_itr, char *start)
 {
+	if (start - chr_itr == 0)
+		return (chr_itr);
 	chr_itr++;
 	return (*chr_itr);	
 }
 
-int tokenize_operator(char *chr_itr, t_token *prev_token)
+/* tokenizes operator and consumes used chars*/
+int tokenize_operator(char **chr_itr, t_token *prev_token)
 {
 	t_token *token;
 	
-	if (*chr_itr == '(')
-		token = add_token(chr_itr, TOKEN_LPAREN, 1, &prev_token);
-	else if (*chr_itr == ')')
-		token = add_token(chr_itr, TOKEN_RPAREN, 1, &prev_token);
-	else if (*chr_itr == '|')
-		token = add_token(chr_itr, TOKEN_PIPE, 1, &prev_token);
-	else if (*chr_itr == '>')
-		token = add_token(chr_itr, TOKEN_IO_OUT, 1, &prev_token);
-	else if (*chr_itr == '<')
-		token = add_token(chr_itr, TOKEN_IO_IN, 1, &prev_token);
-	else if (*chr_itr == '>>')
-		token = add_token(chr_itr, TOKEN_RPAREN, 1, &prev_token);
+	if (**chr_itr == '(')
+		token = add_token(*chr_itr, TOKEN_LPAREN, 1, &prev_token->next);
+	else if (**chr_itr == ')')
+		token = add_token(*chr_itr, TOKEN_RPAREN, 1, &prev_token->next);
+	else if (**chr_itr == '|')
+		token = add_token(*chr_itr, TOKEN_PIPE, 1, &prev_token->next);
+	else if (ft_strncmp(*chr_itr, ">>", 2) == 0)
+		token = add_token(*chr_itr, TOKEN_APPEND, 2, &prev_token->next);
+	else if (ft_strncmp(*chr_itr, "<<", 2) == 0)
+		token = add_token(*chr_itr, TOKEN_HEREDOC, 2, &prev_token->next);
+	else if (**chr_itr == '>')
+		token = add_token(*chr_itr, TOKEN_IO_OUT, 1, &prev_token->next);
+	else if (**chr_itr == '<')
+		token = add_token(*chr_itr, TOKEN_IO_IN, 1, &prev_token->next);
+	*chr_itr += token->cmd.len;
+	return (token);
+}
+
+t_token *tokenize_word(char **chr_itr, t_token *prev_token)
+{
+	t_token *token;
+	
+	// check for double and single quotes
 	return (token);
 }
 
 t_token *scan(int c, char *chr_itr, t_token *prev_token)
 {
-	t_token token;
+	t_token *token;
 	
 	if (ft_strchr("|<>()", c))
-		// tokenize_operator
+		token = tokenize_operator(&chr_itr, prev_token);
 	else
 		//tokenize_word
+	return (token);
+}
+
+void skip_whitespace_between_words(char **chr_itr)
+{
+	while (**chr_itr != '\0' && ft_isspace(**chr_itr) && **chr_itr != '\n')
+		*chr_itr++;
 }
 
 int tokenizer(char *line)
@@ -67,8 +88,10 @@ int tokenizer(char *line)
 	prev_tok = NULL;
 	while (is_not_end(chr_itr))
 	{
-		// skip spaces
-		cur_char = lookahead(chr_itr);
+		skip_whitespace_between_words(&chr_itr);
+		if (!is_not_end(chr_itr))
+			break;
+		cur_char = lookahead(chr_itr, line);
 		// new_tok = scan(prev_tok)
 		prev_tok = new_tok;
 	}
--- ui/token_utils.c
+++ ui/token_utils.c
@@ -6,7 +6,7 @@
 /*   By: dkoca <dkoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 05:48:37 by dkoca             #+#    #+#             */
-/*   Updated: 2024/08/26 07:21:49 by dkoca            ###   ########.fr       */
+/*   Updated: 2024/08/26 12:28:00 by dkoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@ t_token *add_token(char *content, int type, int len, t_token **prev_next_ptr)
 	t_token *new_tok;
 
 	new_tok = ft_calloc(1, sizeof(t_token));
+	if (new_tok == NULL)
+		// safe exit or create safe malloc
+	new_tok->cmd.start = ft_calloc(len + 1, sizeof(char));
 	if (new_tok == NULL)
 		// safe exit or create safe malloc
 	new_tok->cmd.start = content;
