2f0289da14e99ecbab6ea719254bc9f5908224d0
Markus Weiss <markusweiss.mw@gmail.com>
2024-09-02 12:50:39 +0200

    started writing the executor

--- src/debug/debug.c
+++ src/debug/debug.c
@@ -6,7 +6,7 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 15:10:30 by maweiss           #+#    #+#             */
-/*   Updated: 2024/09/02 12:29:44 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/09/02 12:36:44 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ void	ft_deb_commands(t_ms *ms)
 	printf("test for right priority if several redirects are present\n");
 	printf("test if heredocs and input redirects are prioritized right\n");
 	printf("test if redirects to pipes are still there?");
+	ft_scan_cmds(ms);
 }
 
 void	ft_debug(t_ms *ms)
--- src/executor/ms_parenting.c
+++ src/executor/ms_parenting.c
@@ -1,43 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ms_executor.c                                      :+:      :+:    :+:   */
+/*   ms_parenting.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/26 18:15:36 by maweiss           #+#    #+#             */
-/*   Updated: 2024/09/02 12:50:07 by maweiss          ###   ########.fr       */
+/*   Created: 2024/09/02 12:32:33 by maweiss           #+#    #+#             */
+/*   Updated: 2024/09/02 12:32:50 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-void	ft_scan_cmds(t_ms *ms)
-{
-	if (ms->global_flags == 1)
-		ft_here_doc(ms);
-	ft_executor(ms);
-}
-
-void	ft_redir_handler(t_ms *ms, t_cmd_list *curr)
-{
-	t_list_redir	*redirects;
-
-	redirects = curr->cmd->redir;
-	while (redirects)
-	{
-		//handle each redirect at a time;
-	}
-}
-
-void	ft_executor(t_ms *ms)
-{
-	t_cmd_list		*curr;
-
-	curr = ms->cmds;
-	while (curr)
-	{
-		if(curr->cmd->redir)
-			ft_redir_handler(ms, curr);
-	}
-}
