38dbc45fa2b470b400f3455f20418c48d45f89b9
Markus Weiss <markusweiss.mw@gmail.com>
2024-09-19 16:38:44 +0200

    added some insights

--- src/executor/ms_env.c
+++ src/executor/ms_env.c
@@ -6,7 +6,7 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 15:41:22 by maweiss           #+#    #+#             */
-/*   Updated: 2024/09/19 16:14:15 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/09/19 16:38:30 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,15 @@
 - Create a new function that searches for an environmental variable
 - Create a new function that updates an environmental variable
 - Create a new function that exports the environmental variables
+
+Advantage of a hash table over a regular array or linkedlist.
+
+finding a particular value in an array:
+traverse through the array and strncmp the value with the string in the array. time complexity O(n)
+finding a particular value in a linked list:
+traverse through the array and strncmp the value with the string in the array. time complexity O(n)
+finding a particular value in a hash table:
+get the hash value multiplying the string with the hash function. hash value % table size (should be prime) = position in the array.
+time complexity O(1)
+
 */
