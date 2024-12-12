21cf410ef403cfbcd57caa081471c22cc65c9bc7
Markus Weiss <markusweiss.mw@gmail.com>
2024-12-12 19:04:35 +0100

    removed comments

--- headers/ms_executor.h
+++ headers/ms_executor.h
@@ -6,7 +6,7 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 15:50:51 by maweiss           #+#    #+#             */
-/*   Updated: 2024/12/12 11:55:24 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/12/12 19:04:21 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@
 
 typedef struct s_be
 {
-	t_garbage		*garbage;		/*clear and realloc after every execution*/
+	t_garbage		*garbage;
 	int				argc;
-	char			**argv;			/*no need to free*/
-	char			**envp;			/*no need to free*/
-	char			**path;			/*clear at exit*/
-	int				redir_err;		/*error in redirects*/
-	int				nb_cmds;		/*clear after every execution*/
-	int				pipes[2][2];	/*reinit after every execution*/
-	int				saved_std[2];	/*close after every execution*/
-	int				*child_pids;	/*clear and realloc after every execution*/
-	int				*child_ret;		/*return vals of the childs*/
-	int				last_ret;		/*return val of the last command*/
-	char			**builtins;		/*clear at exit*/
-	char			*cwd;			/*clear at exit*/
+	char			**argv;
+	char			**envp;
+	char			**path;
+	int				redir_err;
+	int				nb_cmds;
+	int				pipes[2][2];
+	int				saved_std[2];
+	int				*child_pids;
+	int				*child_ret;
+	int				last_ret;
+	char			**builtins;
+	char			*cwd;
 	t_stab_st		*global_stabs;
 	t_stab			*alphabetic_vars;
 }				t_be;
